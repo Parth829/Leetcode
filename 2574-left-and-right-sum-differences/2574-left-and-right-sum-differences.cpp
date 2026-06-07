a0cc097e48923691760e557d2c7f9fdba2c73136
PARTH <parthdixit283@gmail.com>
2026-06-07 15:41:58 +0530

    Time: 0 ms (100%), Space: 14.9 MB (81.43%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,18 +4,34 @@ public:
         int n = nums.size();
         vector<int> ans(n);
 
-        int leftSum = 0;
-        for (int i = 0; i < n; ++i) {
-            ans[i] = leftSum;
-            leftSum += nums[i];
+        // int leftSum = 0;
+        // for (int i = 0; i < n; ++i) {
+        //     ans[i] = leftSum;
+        //     leftSum += nums[i];
+        // }
+
+        // int rightSum = 0;
+        // for (int i = n - 1; i >= 0; --i) {
+        //     ans[i] = abs(ans[i] - rightSum);
+        //     rightSum += nums[i];
+        // }
+
+        // return ans;
+       
+        int leftsum = 0;
+        for (int i =0;i<n;i++){
+            ans[i]= leftsum;
+            leftsum += nums[i];
         }
 
-        int rightSum = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            ans[i] = abs(ans[i] - rightSum);
-            rightSum += nums[i];
+        int rightsum = 0;
+        for (int i=n-1;i>=0;i--){
+            ans[i] = abs(ans[i] - rightsum);
+            rightsum += nums[i]; 
         }
-
         return ans;
+        
     }
+    
+       
 };
